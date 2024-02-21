ee103f201a55bd255a449fea765e63f6007531e5
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-02-21 22:16:47 +0000

    Added a dark mode/ light mode with toggle button

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ btn.addEventListener('click', () => {
         console.log(data)
         result.innerHTML = `
         <div class="word">
-                <h3>${inputWord}</h3>
+                <h3 id='word'>${inputWord}</h3>
                 <button onclick='playSound()' id='sound-btn'>
                     <i class="fa-solid fa-volume-high"></i>
                 </button>
             </div>
-            <div class="details">
+            <div class="details" id='details'>
                 <p>${data[0].meanings[0].partOfSpeech}</p>
                 <p id='phonetic'>${data[0].phonetics[0].text}</p>
             </div>
-            <p class="definition">
+            <p class="definition" id='definition'>
                 1. ${data[0].meanings[0].definitions[0].definition}
             </p>
             <p class="word-example" id='word-example'>
@@ -32,7 +32,8 @@ btn.addEventListener('click', () => {
 
         const example = document.getElementById('word-example')
         if (data[0].meanings[0].definitions[0].example == null) {
-            example.innerHTML = 'No Example Found.'
+            example.innerHTML = ''
+            example.style.display = 'none'
         }
 
         const phonetic = document.getElementById('phonetic')
@@ -56,7 +57,7 @@ btn.addEventListener('click', () => {
   
     })
     .catch( () => {
-        result.innerHTML = `<h3>No word found!<h3>`
+        result.innerHTML = `<h3 id='error-message'>No word found!<h3>`
     })
 })
 
@@ -64,3 +65,23 @@ function playSound() {
     sound.play();
 }
 
+const checkbox =  document.getElementById('darkmode-toggle')
+
+function toggleDarkMode() {
+    const container = document.getElementById('container')
+    const searchBox =  document.getElementById('input-word')
+    const definition = document.getElementById('definition')
+    const word = document.getElementById('word')
+    const wordExample = document.getElementById('word-example')
+    const errorMessage = document.getElementById('error-message')
+    container.style.backgroundColor = checkbox.checked ? '#242424' : 'white';
+    searchBox.style.backgroundColor = checkbox.checked ? '#242424' : 'white';
+    searchBox.style.color = checkbox.checked ? 'white' : 'black';
+    definition.style.color = checkbox.checked ? 'white' : 'gray';
+    word.style.color = checkbox.checked ? 'white' : 'gray';
+    wordExample.style.color = checkbox.checked ? 'white' : 'gray';
+    errorMessage.style.color = checkbox.checked ? 'white' : 'gray';
+}
+
+
+checkbox.addEventListener('change', toggleDarkMode)
